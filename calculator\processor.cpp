182930fcd2d28b3a57d0a37c7ff9570f35d0192b
Petros Amyan <petrosamyan2003@gmail.com>
2026-03-30 00:51:23 +0400

    Create calculator\processor.cpp

---
+++ calculator\processor.cpp
@@ -0,0 +1,57 @@
+
+#include "processor.h"
+#include <iostream>
+#include <cctype>
+
+int Processor::getValue(const std::string& s) {
+    if (isdigit(s[0]))
+        return std::stoi(s);
+
+    if (s[0] == 'R')
+        return registers[s];
+
+    return varmemory[s];
+}
+
+void Processor::setVariable(const std::string& name, int value) {
+    varmemory[name] = value;
+}
+
+void Processor::execute(const std::vector<Instruction>& commands) {
+    for (const auto& instr : commands) {
+
+        switch (instr.type) {
+
+            case InstrType::MOV:
+                registers[instr.dest] = getValue(instr.source1);
+                break;
+
+            case InstrType::ADD:
+                registers[instr.dest] =
+                    getValue(instr.source1) + getValue(instr.source2);
+                break;
+
+            case InstrType::SUB:
+                registers[instr.dest] =
+                    getValue(instr.source1) - getValue(instr.source2);
+                break;
+
+            case InstrType::MUL:
+                registers[instr.dest] =
+                    getValue(instr.source1) * getValue(instr.source2);
+                break;
+
+            case InstrType::DIV:
+                registers[instr.dest] =
+                    getValue(instr.source1) / getValue(instr.source2);
+                break;
+        }
+    }
+}
+
+void Processor::printRegisters() {
+    std::cout << "\nFinal Registers:\n";
+    for (auto& [k, v] : registers) {
+        std::cout << k << " = " << v << "\n";
+    }
+}
