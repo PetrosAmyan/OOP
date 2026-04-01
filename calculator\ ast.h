6de2ae98ee1b97ff7d49333de051d5bb9e859f40
Petros Amyan <petrosamyan2003@gmail.com>
2026-04-01 15:58:47 +0400

    Update calculator\ ast.h

--- calculator\ ast.h
+++ calculator\ ast.h
@@ -1,40 +1,86 @@
-
 #pragma once
+#include <set>
 #include <string>
+#include <vector>
+#include <map>
+#include <stdexcept>
 
+enum class TokenType {
+    NUMBER, IDENT,
+    PLUS, MINUS, MUL, DIV,
+    LPAREN, RPAREN,
+    ASSIGN, END
+};
+
+struct Token {
+    TokenType type;
+    int value = 0;
+    char name = '\0';
+};
+
+class Lexer {
+private:
+    std::string text;
+    int pos;
+    char current;
+
+    void advance();
+    void skip_whitespace();
+    Token number();
+
+public:
+    Lexer(std::string input_text);
+    std::vector<Token> tokenize();
+};
+
+
+
+enum class NodeType { NUMBER, VARIABLE, OPERATOR, ASSIGN };
 enum class OpType { NONE, ADD, SUB, MUL, DIV };
-enum class NodeType { NUMBER, VARIABLE, OPERATOR };
 
 struct Node {
     NodeType type;
     OpType op;
+    char var;
     int value;
-    std::string name;
-
     Node* left;
     Node* right;
 
     Node(int v);
-    Node(const std::string& n);
+    Node(char c);
     Node(OpType o, Node* l, Node* r);
 };
 
-class Calculator_AST {
-public:
-    Node* parse(const std::string& s);
-
+class Parser {
 private:
-    std::string expr;
+    std::vector<Token> tokens;
     int pos;
+    Token current;
+    Node* root;
 
-    void skip_spaces();
-    void check_bounds();
+    void advance();
+    Node* expr();
+    Node* term();
+    Node* factor();
+    Node* statement();
+    void deleteTree(Node* node);
 
-    Node* parse_identifier();
-    Node* parse_number();
-    Node* parse_sub_add();
-    Node* parse_mul_div();
-    Node* parse_val();
+public:
+    Parser();
+    ~Parser();
+
+    Node* parse(std::vector<Token> t);
+    void collectVariables(Node* node, std::set<char>& vars);
 };
 
-void free_tree_recursive(Node* node);
+
+
+class SymbolTable {
+private:
+    std::map<char, int> table;
+
+public:
+    void set(const char& name, int value);
+    int get(const char& name) const;
+    bool exists(const char& name) const;
+};
