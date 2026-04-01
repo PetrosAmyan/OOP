c7bdb84c65a0b438182be0b98b7ea927c60d300c
Petros Amyan <petrosamyan2003@gmail.com>
2026-04-01 16:03:54 +0400

    Update calculator\ir.cpp

--- calculator\ir.cpp
+++ calculator\ir.cpp
@@ -6,6 +6,23 @@ std::string InstGenerate::newreg() {
 }
 
 std::string InstGenerate::generate(Node* node) {
+		if (node->type == NodeType::ASSIGN) {
+    std::string rhs = generate(node->right);
+
+    //std::string varName = node->left->var;
+		std::string varName(1, node->left->var);
+    commands.push_back({
+        InstrType::MOV,
+        varName,  
+        rhs,
+        ""
+    });
+
+    return varName;
+}
+
+
+
     if (node->type == NodeType::NUMBER) {
         std::string reg = newreg();
         commands.push_back({InstrType::MOV, reg, std::to_string(node->value), ""});
@@ -13,10 +30,15 @@ std::string InstGenerate::generate(Node* node) {
     }
 
     if (node->type == NodeType::VARIABLE) {
-        std::string reg = newreg();
-        commands.push_back({InstrType::MOV, reg, node->name, ""});
-        return reg;
-    }
+    std::string reg = newreg();
+    commands.push_back({
+        InstrType::MOV,
+        reg,
+        std::string(1, node->var),
+        ""
+    });
+    return reg;
+}
 
     std::string left = generate(node->left);
     std::string right = generate(node->right);
