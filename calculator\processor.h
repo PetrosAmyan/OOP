e3a3ea92aa5929bb79769a27b0daafa0cf692954
Petros Amyan <petrosamyan2003@gmail.com>
2026-04-01 16:07:09 +0400

    Update calculator\processor.h

--- calculator\processor.h
+++ calculator\processor.h
@@ -1,18 +1,45 @@
-#pragma once
-#include <unordered_map>
-#include <vector>
-#include <string>
+#include <iostream>
+#include "ast.h"
 #include "ir.h"
+#include "processor.h"
 
-class Processor {
-private:
-    std::unordered_map<std::string, int> registers;
-    std::unordered_map<std::string, int> varmemory;
+int main() {
+    std::string input;
+    std::cout << "please enter expression: ";
+    std::getline(std::cin, input);
 
-    int getValue(const std::string& s);
+    Lexer lexer(input);
+    std::vector<Token> tokens = lexer.tokenize();
+    
+    Parser parser;
+    Node* root = parser.parse(tokens);
 
-public:
-    void setVariable(const std::string& name, int value);
-    void execute(const std::vector<Instruction>& commands);
-    void printRegisters();
-};
+    
+    std::set<char> vars;
+    parser.collectVariables(root, vars);
+
+
+    SymbolTable symTable;
+
+    for (char v : vars) {
+        int val;
+        std::cout << "Enter value for " << v << " variable  ";
+        std::cin >> val;
+        symTable.set(v, val);
+    }
+
+    
+    InstGenerate ir;
+    std::vector<Instruction> commands = ir.createIR(root);
+
+    Processor proc;
+
+    for (char v : vars) {
+        proc.setVariable(std::string(1, v), symTable.get(v));
+    }
+
+    proc.execute(commands);
+    proc.printRegisters();
+
+    return 0;
+}
